92c1f5d7df503c4ce3d820bb231d83e4fe6e4a11
PC <PC@DESKTOP-R4P1PJC>
2023-11-16 22:38:25 +0100

    Add project files.

---
+++ HW6-2-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SumInRange
+{
+    class Program
+    {
+        static int SumBetween(int a, int b)
+        {
+            int sum = 0;
+            int start = Math.Min(a, b);
+            int end = Math.Max(a, b);
+
+            
+            for (int i = start; i <=end; i++)
+            {
+                sum += i;
+            }
+
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое целое число:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе целое число:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = SumBetween(num1, num2);
+            Console.WriteLine($"Сумма чисел в диапазоне между {num1} и {num2} равна {result}");
+        }
+    }
+}
